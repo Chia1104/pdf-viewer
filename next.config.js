c51d032c29f022b8fc82deb6f6a829d5542a12cf
Chia1104 <yuyuchia7423@gmail.com>
2022-11-14 18:27:07 +0800

    :adhesive_bandage: Update `next.config.js`.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,45 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+const securityHeaders = [
+  {
+    key: "X-DNS-Prefetch-Control",
+    value: "on",
+  },
+  {
+    key: "X-XSS-Protection",
+    value: "1; mode=block",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "SAMEORIGIN",
+  },
+  {
+    key: "Referrer-Policy",
+    value: "origin-when-cross-origin",
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+];
 
-module.exports = nextConfig
+const nextConfig = {
+  swcMinify: true,
+  reactStrictMode: true,
+  output: "standalone",
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  async redirects() {
+    return [];
+  },
+  images: {},
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: securityHeaders,
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
