ae4e37dc62de1d1650ae8a6a7868ecedd71bfaaa
Chia1104 <yuyuchia7423@gmail.com>
2022-11-16 01:55:28 +0800

    :wastebasket: Remove `unstable_getServerSession`.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,16 +4,16 @@ import { type GetServerSideProps } from "next";
 import { authOptions } from "@/pages/api/auth/[...nextauth]";
 import { unstable_getServerSession } from "next-auth/next";
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const session = await unstable_getServerSession(
-    context.req,
-    context.res,
-    authOptions
-  );
-  return {
-    props: { session },
-  };
-};
+// export const getServerSideProps: GetServerSideProps = async (context) => {
+//   const session = await unstable_getServerSession(
+//     context.req,
+//     context.res,
+//     authOptions
+//   );
+//   return {
+//     props: { session },
+//   };
+// };
 
 const Home = () => {
   return (
