316fee7df237b9605907c85b7244ca1a9f21f88a
Kelly <redbeeno.1@gmail.com>
2022-11-21 11:02:45 +0800

    add error page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,24 +1,5 @@
-import { type GetServerSideProps } from "next";
-import { getServerSession } from "@/utils/get-server-session.util";
 import { Card, Button } from "@/components";
 import { useRouter } from "next/router";
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const session = await getServerSession(context);
-
-  if (!session) {
-    return {
-      redirect: {
-        destination: "/",
-        permanent: false,
-      },
-    };
-  }
-
-  return {
-    props: {},
-  };
-};
 
 const HistoryPage = () => {
   const router = useRouter();
@@ -26,8 +7,9 @@ const HistoryPage = () => {
   return (
     <div className="relative mt-20 h-[calc(100vh-80px)]">
       <Card className="absolute top-[50%] translate-y-[-50%] mx-auto py-5 w-1/2 text-center text-primary font-semibold">
-        <p>~此功能尚未完工~</p>
-        <p>敬啟期待</p>
+        <p className="text-xl">
+          Oops! <br /> 查無此頁
+        </p>
         <Button className="mx-auto mt-5 py-1" onClick={() => router.push("/")}>
           回首頁
         </Button>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,7 @@ const Home = () => {
     <Page className="c-container flex items-center justify-center min-h-screen">
       <Head title={meta.title} description={meta.description} />
       <article className="pt-[80px] sm:pt-0">
-        <Card className="min-h-screen sm:min-h-fit w-screen sm:w-full max-w-[824px] py-6 text-primary font-semibold border-0 sm:border sm:rounded-[20px] rounded-none">
+        <Card className="min-h-screen sm:min-h-fit w-screen sm:w-full max-w-[824px] py-6 text-primary font-semibold border-0 sm:border rounded-none sm:rounded-[20px]">
           <p className="py-3 sm:py-0 md:pl-20 sm:pb-6 text-center md:text-start text-xl md:text-base border-b border-solid border-[rgba(127, 171, 190, 0.6)]">
             簡單、快速3步驟
             <br />
