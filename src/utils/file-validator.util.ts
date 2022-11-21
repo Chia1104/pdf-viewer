ec2ad5fc2afd9b561c38e40c107b4e5b26b1136b
Chia1104 <yuyuchia7423@gmail.com>
2022-11-21 11:15:15 +0800

    :adhesive_bandage: Simple fix for some non-critical issue.

--- src/utils/file-validator.util.ts
+++ src/utils/file-validator.util.ts
@@ -2,14 +2,14 @@ import { z } from "zod";
 
 const MAX_FILE_SIZE = 10000000; // 10MB
 const ACCEPTED_IMAGE_TYPES = [
-  "image/jpeg",
-  "image/jpg",
-  "image/png",
-  "image/webp",
+  // "image/jpeg",
+  // "image/jpg",
+  // "image/png",
+  // "image/webp",
   "application/pdf",
 ];
 const MESSAGE = {
-  INVALID_FILE_TYPE: "請放入有效的檔案(PDF / Image)",
+  INVALID_FILE_TYPE: "請放入有效的檔案(PDF)",
   INVALID_FILE_SIZE: "檔案大小不可超過10MB",
   SUCCESS: "檔案上傳成功",
 };
